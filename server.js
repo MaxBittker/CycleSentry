b079016d8e3245d1f7363da715b825c7a94184d2
maxbittker <maxbittker@gmail.com>
2015-10-27 11:14:43 -0400

    mongo

--- server.js
+++ server.js
@@ -10,53 +10,27 @@ var express = require('express'),
     // favicon = require('serve-favicon'),
     fs = require('fs');
 
-var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
-
-// Connection URL 
-var url = 'mongodb://localhost:27017/Cycle';
-// Use connect method to connect to the Server 
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-
-
-});
-
-
-
-var insertDocument = function(db, data, callback) {
-    db.collection('testCollection').insertOne({
-        text: data
-    }, function(err, result) {
-        assert.equal(err, null);
-        console.log("Inserted a document into the testCollection.");
-        callback(result);
-    });
-};
-
-var findDocuments = function(db, callback) {
-  // Get the documents collection 
-  var collection = db.collection('testCollection');
-  // Find some documents 
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    assert.equal(2, docs.length);
-    console.log("Found the following records");
-    console.dir(docs);
-    callback(docs);
-  });
-}
+var Db = require('mongodb').Db,
+    Server = require('mongodb').Server,
+    ObjectId = require('mongodb').ObjectID;
 
 
 var createServer = function(port) {
 
     // app.use(favicon(__dirname + '/favicon.ico'));
+    var db = new Db('Cycle', new Server('localhost', 27017));
+    var collection = db.collection("testCollection");
+
+
+
 
     app.get('/', function(req, res) {
 
         res.set('Content-Type', 'text/html');
-        res.send('<h4>DATA:<br>' + data.join('<br>') + '</h4>');
+
+        res.send('<h4>DATA:<br>' + JSON.parasecollection.find({}).toString() + '</h4>');
+        db.close();
+
 
     })
 
@@ -64,14 +38,23 @@ var createServer = function(port) {
 
         var text = req.params.text.toString();
         console.log(text);
-       
-        insertDocument(db, "helloworld", function() {
-            db.close();
+
+        db.open(function(err, db) {
+            // Insert a single document
+            // console.log(text.toString());
+            collection.insert({
+                text: text.toString()
+            }, function(err, docsInserted) {
+                db.close();
+                // get last id
+                res.send(docsInserted.ops[0]._id);
+            });
+
         });
 
-        res.send(text);
 
     });
+
 
 
     app.use('/static/', express.static(path.join(__dirname, 'public')));
